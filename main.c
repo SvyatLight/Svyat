3443770f64ea9fe6f02ff8f83cb607267d9a5f09
SvyatLight <pavel.tozik@yandex.ru>
2018-05-03 13:51:35 +0300

    f

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <std.h>
-
+//ghfgwegw
 int main (vaid)
 {	int a,b;
 
 	printf("Input 2 whole numbers");
 	scanf("%d%d", &a,&b);
 	printf("a + b = %d + % d = ,a,b, a+b");
+	printf("a - b = %d - % d = ,a,b, a-b");
 }
