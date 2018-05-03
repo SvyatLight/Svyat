fcb71986e6ff1a05626c264489772afd3cff8e90
SvyatLight <pavel.tozik@yandex.ru>
2018-05-03 13:47:58 +0300

    Revert "dd"
    This reverts commit f92b1c10053b971820f76136adcf403b9623dba5.

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <std.h>
-//ghfgwegw
+
 int main (vaid)
 {	int a,b;
+
 	printf("Input 2 whole numbers");
 	scanf("%d%d", &a,&b);
 	printf("a + b = %d + % d = ,a,b, a+b");
-	printf("a - b = %d - % d = ,a,b, a-b");
-	printf("a / b = %d / % d = ,a,b, a/b");
 }
